461a9a6cde6f7ca04e496d5e0daab99bd61ce77a
Rohit Chakraborty <rohitchak68@gmail.com>
2025-01-22 17:58:48 +0530

    delete task

--- frontend/src/contexts/BoardContext.jsx
+++ frontend/src/contexts/BoardContext.jsx
@@ -18,8 +18,18 @@ const BoardProvider = ({children}) => {
         });
       };
 
+    const deleteTask = (task) => {
+        setTasks((prev) => {
+            const updatedTasks = prev[task.status].filter((t)=> task.id !== t.id)
+            return {
+                ...prev,
+                [task.status]: updatedTasks
+            }
+        })
+    }
+
     return (
-        <BoardContext.Provider value={{tasks, moveTask}}>
+        <BoardContext.Provider value={{tasks, moveTask, deleteTask}}>
             {children}
         </BoardContext.Provider>
     )
