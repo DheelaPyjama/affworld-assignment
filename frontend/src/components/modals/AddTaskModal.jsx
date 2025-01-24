e065335d61651823c0ff20387e7b9d30a1f79b31
Rohit Chakraborty <rohitchak68@gmail.com>
2025-01-24 19:13:40 +0530

    modal layout

--- frontend/src/components/modals/AddTaskModal.jsx
+++ frontend/src/components/modals/AddTaskModal.jsx
@@ -1,18 +1,53 @@
 import React, { useContext } from 'react'
 import { BoardContext } from '../../contexts/BoardContext'
-import { Dialog } from '@headlessui/react'
+import { Dialog, DialogTitle } from '@headlessui/react'
 
 const AddTaskModal = () => {
-  const { isModalOpen, closeModal, addTask } = useContext(BoardContext)
+  const { closeModal, addTask } = useContext(BoardContext)
   return (
     <div className='relative'>
-      {isModalOpen && (
-        <Dialog
-          open={isModalOpen}
-          onClose={closeModal}
-          className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50'
-        ></Dialog>
-      )}
+      <Dialog
+        open={isModalOpen}
+        onClose={closeModal}
+        className='fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50'
+      >
+        <div className='bg-white rounded-lg p-6 max-w-sm w-full shadow-lg'>
+          <DialogTitle className='text-lg font-bold mb-4'>Add a new Task</DialogTitle>
+          <form>
+            <div className='mb-4'>
+              <label htmlFor='taskName' className='block text-sm font-medium text-gray-700'>
+                Task Description
+              </label>
+              <input
+                id='taskName'
+                type='text'
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500'
+              />
+            </div>
+            <div className='mb-4'>
+              <label htmlFor='status' className='block text-sm font-medium text-gray-700'>
+                Status
+              </label>
+              <select
+                id='status'
+                className='mt-1 block w-full p-2 border border-gray-300 rounded-md shadow-sm focus:ring-green-500 focus:border-green-500'
+              >
+                <option value='pending'>Pending</option>
+                <option value='inProgress'>In Progress</option>
+                <option value='completed'>Completed</option>
+              </select>
+            </div>
+            <div className='flex justify-end'>
+              <button type='button' onClick={closeModal} className='mr-2 px-4 py-2 bg-red-800 text-white rounded-md'>
+                Cancel
+              </button>
+              <button type='button' onClick={addTask(task)}>
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
+      </Dialog>
     </div>
   )
 }
