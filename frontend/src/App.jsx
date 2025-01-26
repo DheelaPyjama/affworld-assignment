64775f4007e769927b214ddceb399bda40cefc3d
Rohit Chakraborty <rohitchak68@gmail.com>
2025-01-26 21:11:25 +0530

    added routes with react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Board from './components/Board'
 import BoardProvider from './contexts/BoardContext'
 import Post from './components/Post'
 import Feed from './components/Feed'
+import AuthProvider, { useAuth } from './contexts/AuthContext'
+
+const ProtectedRoute = ({ children }) => {
+  const { user } = useAuth()
+  if (!user) {
+    return <Navigate to='/login' replace />
+  }
+  return children
+}
 
 function App() {
   const [posts, setPosts] = useState([])
@@ -14,19 +24,37 @@ function App() {
   }
 
   return (
-    <>
-      <Login />
-      <Register />
+    <AuthProvider>
       <BoardProvider>
-        <Board />
+        <Router>
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route
+              path='/board'
+              element={
+                <ProtectedRoute>
+                  <Board />
+                </ProtectedRoute>
+              }
+            ></Route>
+            <Route
+              path='/feed'
+              element={
+                <ProtectedRoute>
+                  <div className='min-h-screen bg-gray-100 p-6'>
+                    <div className='max-w-md mx-auto'>
+                      <Post onSubmit={handleSubmit} />
+                      <Feed posts={posts} />
+                    </div>
+                  </div>
+                </ProtectedRoute>
+              }
+            ></Route>
+          </Routes>
+        </Router>
       </BoardProvider>
-      <div className='min-h-screen bg-gray-100 p-6'>
-        <div className='max-w-md mx-auto'>
-          <Post onSubmit={handleSubmit} />
-          <Feed posts={posts} />
-        </div>
-      </div>
-    </>
+    </AuthProvider>
   )
 }
 
