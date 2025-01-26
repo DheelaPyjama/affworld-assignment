865e48cb2a7e7c8a660a214bf5bb355982a70ae6
Rohit Chakraborty <rohitchak68@gmail.com>
2025-01-26 19:54:31 +0530

    feed ui

---
+++ frontend/src/components/Feed.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Feed = ({ posts }) => {
+  return (
+    <div className='space-y-6'>
+      {posts.map((post) => (
+        <div className='bg-white p-6 rounded-lg shadow-md'>
+          <img src={post.image} alt={post.caption} className='w-full h-64 object-cover rounded-md mb-4' />
+          <p className='text-gray-700'>{post.caption}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
